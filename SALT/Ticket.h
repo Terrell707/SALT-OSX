43aa21e7c4863e5061f86678527d5d94ffda1644
Terrell707 <adr510909@gmail.com>
2015-02-11 17:41:13 -0800

    Add attributes from database

--- SALT/Ticket.h
+++ SALT/Ticket.h
@@ -7,34 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <CoreData/CoreData.h>
 
 @class Employee, Judge, Site, Witness;
 
-@interface Ticket : NSManagedObject
+@interface Ticket : NSObject
 
-@property (nonatomic, retain) NSNumber * ticket_no;
-@property (nonatomic, retain) NSDate * order_date;
-@property (nonatomic, retain) NSString * call_order_no;
-@property (nonatomic, retain) NSString * first_name;
-@property (nonatomic, retain) NSString * last_name;
-@property (nonatomic, retain) NSString * bpa_no;
-@property (nonatomic, retain) NSNumber * can;
-@property (nonatomic, retain) NSString * tin;
-@property (nonatomic, retain) NSString * soc;
-@property (nonatomic, retain) NSDate * hearing_date;
-@property (nonatomic, retain) NSString * hearing_time;
-@property (nonatomic, retain) NSString * status;
-@property (nonatomic, retain) NSNumber * emp_worked;
-@property (nonatomic, retain) NSNumber * judge_presided;
-@property (nonatomic, retain) NSString * at_site;
-@property (nonatomic, retain) Employee *workedBy;
-@property (nonatomic, retain) Judge *judgePresided;
-@property (nonatomic, retain) Site *heldAt;
-@property (nonatomic, retain) NSSet *helpedBy;
-@end
-
-@interface Ticket (CoreDataGeneratedAccessors)
+@property (readwrite, copy) NSNumber * ticket_no;
+@property (readwrite, copy) NSDate * order_date;
+@property (readwrite, copy) NSString * call_order_no;
+@property (readwrite, copy) NSString * first_name;
+@property (readwrite, copy) NSString * last_name;
+@property (readwrite, copy) NSString * bpa_no;
+@property (readwrite, copy) NSNumber * can;
+@property (readwrite, copy) NSString * tin;
+@property (readwrite, copy) NSString * soc;
+@property (readwrite, copy) NSDate * hearing_date;
+@property (readwrite, copy) NSString * hearing_time;
+@property (readwrite, copy) NSString * status;
+@property (readwrite, copy) NSNumber * emp_worked;
+@property (readwrite, copy) NSNumber * judge_presided;
+@property (readwrite, copy) NSString * at_site;
+@property (readwrite, copy) Employee *workedBy;
+@property (readwrite, copy) Judge *judgePresided;
+@property (readwrite, copy) Site *heldAt;
+@property (readwrite, copy) NSSet *helpedBy;
 
 - (void)addHelpedByObject:(Witness *)value;
 - (void)removeHelpedByObject:(Witness *)value;
--- SALT/Ticket.m
+++ SALT/Ticket.m
@@ -15,24 +15,58 @@
 
 @implementation Ticket
 
-@dynamic ticket_no;
-@dynamic order_date;
-@dynamic call_order_no;
-@dynamic first_name;
-@dynamic last_name;
-@dynamic bpa_no;
-@dynamic can;
-@dynamic tin;
-@dynamic soc;
-@dynamic hearing_date;
-@dynamic hearing_time;
-@dynamic status;
-@dynamic emp_worked;
-@dynamic judge_presided;
-@dynamic at_site;
-@dynamic workedBy;
-@dynamic judgePresided;
-@dynamic heldAt;
-@dynamic helpedBy;
+@synthesize ticket_no;
+@synthesize order_date;
+@synthesize call_order_no;
+@synthesize first_name;
+@synthesize last_name;
+@synthesize bpa_no;
+@synthesize can;
+@synthesize tin;
+@synthesize soc;
+@synthesize hearing_date;
+@synthesize hearing_time;
+@synthesize status;
+@synthesize emp_worked;
+@synthesize judge_presided;
+@synthesize at_site;
+@synthesize workedBy;
+@synthesize judgePresided;
+@synthesize heldAt;
+@synthesize helpedBy;
 
+- (id)init {
+    self = [super init];
+    if (self) {
+        // Sets defaults for newly created Tickets.
+        hearing_date = [NSDate date];
+    }
+    
+    return self;
+}
+
+- (id)initWithData:(NSDictionary *)data
+{
+    self = [super init];
+    if (self) {
+        // Creates a ticket out of data from a json object.
+        ticket_no = data[@"ticket_no"];
+        order_date = data[@"order_date"];
+        call_order_no = data[@"call_order_no"];
+        first_name = data[@"first_name"];
+        last_name = data[@"last_name"];
+        bpa_no = data[@"bpa_no"];
+        can = data[@"can"];
+        tin = data[@"tin"];
+        soc = data[@"soc"];
+        hearing_date = data[@"hearing_date"];
+        hearing_time = data[@"hearing_time"];
+        status = data[@"status"];
+        emp_worked = data[@"emp_worked"];
+        judge_presided = data[@"judge_presided"];
+        at_site = data[@"at_site"];
+    }
+    
+    return self;
+}
 @end
