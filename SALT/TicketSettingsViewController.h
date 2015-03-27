6143c4e19a9e293dbef006bb9f5c91bf652491c0
Terrell707 <adr510909@gmail.com>
2015-03-26 19:45:58 -0700

    Controls range of dates of tickets shown, and the columns to display

---
+++ SALT/TicketSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TicketSettingsViewController.h
+//  SALT
+//
+//  Created by Adrian T. Chambers on 3/26/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TicketSettingsViewController : NSViewController
+
+@end
---
+++ SALT/TicketSettingsViewController.m
@@ -0,0 +1,22 @@
+//
+//  TicketSettingsViewController.m
+//  SALT
+//
+//  Created by Adrian T. Chambers on 3/26/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import "TicketSettingsViewController.h"
+
+@interface TicketSettingsViewController ()
+
+@end
+
+@implementation TicketSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
