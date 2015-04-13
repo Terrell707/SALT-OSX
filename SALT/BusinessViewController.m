c3ee5f86b51390c7b12eb277a2982edb2ffbc783
Terrell707 <adr510909@gmail.com>
2015-04-13 14:45:47 -0700

    Set up for the Business tab.

---
+++ SALT/BusinessViewController.m
@@ -0,0 +1,22 @@
+//
+//  BusinessViewController.m
+//  SALT
+//
+//  Created by Adrian T. Chambers on 4/11/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import "BusinessViewController.h"
+
+@interface BusinessViewController ()
+
+@end
+
+@implementation BusinessViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
