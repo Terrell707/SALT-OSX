fd070f50ccc3689fb64f12736a1374688078d8ae
Terrell707 <adr510909@gmail.com>
2015-04-20 11:01:16 -0700

    Added new tests.

--- SALTTests/FieldFormatterTests.m
+++ SALTTests/FieldFormatterTests.m
@@ -27,7 +27,7 @@
     [super tearDown];
 }
 
-- (void)testFormatFirstName {
+- (void)testFormatName {
     NSString *firstName = @"First";
     NSString *lastName = @"Last";
     NSString *expectedName, *formattedName;
@@ -55,50 +55,109 @@
     name = @"Last, First";
     actualNameDict = [_fieldFormatter unformatName:name];
     expectedNameDict = [NSDictionary dictionaryWithObjectsAndKeys:@"Last", @"last_name", @"First", @"first_name", nil];
-    XCTAssertEqualObjects(actualNameDict, expectedNameDict, "Last Name first unformatted name returned does not have same keys and values.");
+    XCTAssertEqualObjects(actualNameDict, expectedNameDict, @"Last Name first unformatted name returned does not have same keys and values.");
     
     // Tests that name is unformatted correctly with first name first.
     [_fieldFormatter setLastNameFirst:NO];
     name = @"First Last";
     actualNameDict = [_fieldFormatter unformatName:name];
     expectedNameDict = [NSDictionary dictionaryWithObjectsAndKeys:@"Last", @"last_name", @"First", @"first_name", nil];
-    XCTAssertEqualObjects(actualNameDict, expectedNameDict, "First Name first unformatted returned does not have same keys and values.");
+    XCTAssertEqualObjects(actualNameDict, expectedNameDict, @"First Name first unformatted returned does not have same keys and values.");
 }
 
-- (void)testCallOrderBPAFormat {
+- (void)testTicketNumber {
+    NSString *ticketNumber;
+    NSString *expectedNumber;
+    NSString *actualNumber;
+    
+    ticketNumber = @"";
+    expectedNumber = @"";
+    actualNumber = [_fieldFormatter ticketNumberFormat:ticketNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted Ticket Number should be blank.");
+    
+    ticketNumber = @"1234567";
+    expectedNumber = @"1234567";
+    actualNumber = [_fieldFormatter ticketNumberFormat:ticketNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted Ticket Number shouldn't of been changed.");
+    
+    ticketNumber = @"123456789";
+    expectedNumber = @"12345678";
+    actualNumber = [_fieldFormatter ticketNumberFormat:ticketNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted Ticket Number should of been truncated by one char.");
+}
+
+- (void)testCallOrderFormat {
+    NSString *callNumber;
+    NSString *expectedCallNumber;
+    NSString *actualCallNumber;
+    
+    callNumber = @"1234567890123";
+    expectedCallNumber = @"1234-56-7890123";
+    actualCallNumber = [_fieldFormatter callOrderFormat:callNumber];
+    XCTAssertEqualObjects(actualCallNumber, expectedCallNumber, @"Formatted Call Order Number should not be truncated.");
+    
+    callNumber = @"1234567890123456";
+    expectedCallNumber = @"1234-56-7890123";
+    actualCallNumber = [_fieldFormatter callOrderFormat:callNumber];
+    XCTAssertEqualObjects(actualCallNumber, expectedCallNumber, @"Formatted Call Order Number should be a max of 20 chars.");
+}
+
+- (void)testBPAFormat {
     NSString *bpaNumber;
-    NSString *expectedBPA;
-    NSString *actualBPA;
-    
-    bpaNumber = @"";
-    expectedBPA = @"";
-    actualBPA = [_fieldFormatter callOrderFormat:bpaNumber];
-    XCTAssertEqualObjects(actualBPA, expectedBPA, "Formatted BPA Number should be blank.");
-    
-    bpaNumber = @"12345";
-    expectedBPA = @"1234-5";
-    actualBPA = [_fieldFormatter callOrderFormat:bpaNumber];
-    XCTAssertEqualObjects(actualBPA, expectedBPA, "Formatted BPA Number does not have hyphen after 4th char.");
-    
-    bpaNumber = @"1234";
-    expectedBPA = @"1234";
-    actualBPA = [_fieldFormatter callOrderFormat:bpaNumber];
-    XCTAssertEqualObjects(actualBPA, expectedBPA, "Formatted BPA Number should not have any hyphens.");
-    
-    bpaNumber = @"123456";
-    expectedBPA = @"1234-56";
-    actualBPA = [_fieldFormatter callOrderFormat:bpaNumber];
-    XCTAssertEqualObjects(actualBPA, expectedBPA, "Formatted BPA Number should only have hyphen after 4th char.");
-    
-    bpaNumber = @"1234567";
-    expectedBPA = @"1234-56-7";
-    actualBPA = [_fieldFormatter callOrderFormat:bpaNumber];
-    XCTAssertEqualObjects(actualBPA, expectedBPA, "Formatted BPA Number should have hyphen after 4th and 7th char.");
+    NSString *expectedNumber;
+    NSString *actualNumber;
     
     bpaNumber = @"12345678901";
-    expectedBPA = @"1234-56-78901";
-    actualBPA = [_fieldFormatter callOrderFormat:bpaNumber];
-    XCTAssertEqualObjects(actualBPA, expectedBPA, "Formatted BPA Number should have hyphen after 4th and 7th char.");
+    expectedNumber = @"1234-56-78901";
+    actualNumber = [_fieldFormatter bpaFormat:bpaNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted BPA Number should not be truncated.");
+    
+    bpaNumber = @"123456789012";
+    expectedNumber = @"1234-56-78901";
+    actualNumber = [_fieldFormatter bpaFormat:bpaNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted BPA Number should be truncated by one char.");
+}
+
+// Tests that test both call order and bpa format.
+- (void)testBPAandCallOrderFormat {
+    NSString *text;
+    NSString *expectedText;
+    NSString *actualText;
+    
+    text = @"";
+    expectedText = @"";
+    actualText = [_fieldFormatter callOrderFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted Call Order/BPA should be blank.");
+    
+    text = @"12345";
+    expectedText = @"1234-5";
+    actualText = [_fieldFormatter callOrderFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted Call Order/BPA does not have hyphen after 4th char.");
+    
+    text = @"1234";
+    expectedText = @"1234";
+    actualText = [_fieldFormatter callOrderFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted Call Order/BPA should not have any hyphens.");
+    
+    text = @"123456";
+    expectedText = @"1234-56";
+    actualText = [_fieldFormatter callOrderFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted Call Order/BPA should only have hyphen after 4th char.");
+    
+    text = @"1234567";
+    expectedText = @"1234-56-7";
+    actualText = [_fieldFormatter callOrderFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted Call Order/BPA should have hyphen after 4th and 7th char.");
+    
+    text = @"12345678901";
+    expectedText = @"1234-56-78901";
+    actualText = [_fieldFormatter callOrderFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted Call Order/BPA should have hyphen after 4th and 7th char.");
+    
+    text = @"12-3";
+    expectedText = @"123";
+    actualText = [_fieldFormatter callOrderFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted Call Order/BPA should remove out of place hyphen.");
 }
 
 - (void)testTinFormat {
@@ -109,17 +168,80 @@
     tinNumber = @"";
     expectedTin = @"";
     actualTin = [_fieldFormatter tinFormat:tinNumber];
-    XCTAssertEqualObjects(actualTin, expectedTin, "Formatted Tin Number should be blank.");
+    XCTAssertEqualObjects(actualTin, expectedTin, @"Formatted Tin Number should be blank.");
     
     tinNumber = @"1234";
     expectedTin = @"1234";
     actualTin = [_fieldFormatter tinFormat:tinNumber];
-    XCTAssertEqualObjects(actualTin, expectedTin, "Formatted Tin Number should have no hyphens.");
+    XCTAssertEqualObjects(actualTin, expectedTin, @"Formatted Tin Number should have no hyphens.");
     
     tinNumber = @"12345";
     expectedTin = @"1234-5";
     actualTin = [_fieldFormatter tinFormat:tinNumber];
-    XCTAssertEqualObjects(actualTin, expectedTin, "Formatted Tin Number should have a hyphen after 4th char.");
+    XCTAssertEqualObjects(actualTin, expectedTin, @"Formatted Tin Number should have a hyphen after 4th char.");
+}
+
+- (void)testNameFormat {
+    NSString *name;
+    NSString *expectedName;
+    NSString *actualName;
+    
+    name = @"";
+    expectedName = @"";
+    actualName = [_fieldFormatter nameFormat:name];
+    XCTAssertEqualObjects(actualName, expectedName, @"Formatted Name should be blank.");
+    
+    name = @"Robinson";
+    expectedName = @"Robinson";
+    actualName = [_fieldFormatter nameFormat:name];
+    XCTAssertEqualObjects(actualName, expectedName, @"Formatted Name should be returned the same.");
+    
+    name = @"Robinson-Floyd-Chambers";
+    expectedName = @"Robinson-Floyd-Chamb";
+    actualName = [_fieldFormatter nameFormat:name];
+    XCTAssertEqualObjects(actualName, expectedName, @"Formatted Name should be truncated by three chars.");
+}
+
+- (void)testCANFormat {
+    NSString *canNumber;
+    NSString *expectedNumber;
+    NSString *actualNumber;
+    
+    canNumber = @"";
+    expectedNumber = @"";
+    actualNumber = [_fieldFormatter canFormat:canNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted CAN Number should be blank.");
+    
+    canNumber = @"4003932";
+    expectedNumber = @"4003932";
+    actualNumber = [_fieldFormatter canFormat:canNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted CAN Number should be returned the same.");
+    
+    canNumber = @"400393201";
+    expectedNumber = @"4003932";
+    actualNumber = [_fieldFormatter canFormat:canNumber];
+    XCTAssertEqualObjects(actualNumber, expectedNumber, @"Formatted CAN Number should be truncated by 2 chars.");
+}
+
+- (void)testSOCFormat {
+    NSString *text;
+    NSString *expectedText;
+    NSString *actualText;
+    
+    text = @"";
+    expectedText = @"";
+    actualText = [_fieldFormatter socFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted SOC should be blank.");
+    
+    text = @"252E";
+    expectedText = @"252E";
+    actualText = [_fieldFormatter socFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted SOC should be returned the same.");
+    
+    text = @"252EX";
+    expectedText = @"252E";
+    actualText = [_fieldFormatter socFormat:text];
+    XCTAssertEqualObjects(actualText, expectedText, @"Formatted SOC should be truncated by 1 char.");
 }
 
 @end
