132678c9647b2e70e10a3b1a30ae34541a85a358
Terrell707 <adr510909@gmail.com>
2015-04-22 15:39:18 -0700

    Updated Ticket updates Witnesses.

--- SALT/InsertTicketViewController.m
+++ SALT/InsertTicketViewController.m
@@ -314,11 +314,6 @@
         [newTicket addHelpedByObject:expert];
     }
     
-    NSLog(@"Experts associated with this ticket.");
-    for (Expert *e in newTicket.helpedBy) {
-        NSLog(@"%@", e.first_name);
-    }
-    
     // If the user clicked the "Add" button, this will create a new hearing Ticket.
     if (_updateTicket == NO) {
         BOOL inserted = [[DataController sharedDataController] insertTicket:newTicket];
@@ -355,7 +350,48 @@
     else {
         BOOL updated = [[DataController sharedDataController] updateTicket:_oldTicket withChanges:newTicket];
         if (updated) {
-            NSLog(@"Ticket was updated.");
+            NSLog(@"Ticket was successfully updated!");
+            
+            // Creates an array of witnesses that were removed from the ticket.
+            NSMutableArray *oldWitnesses = [NSMutableArray arrayWithArray:[_oldTicket.helpedBy allObjects]];
+            [oldWitnesses removeObjectsInArray:[newTicket.helpedBy allObjects]];
+            // Creates an array of witnesses that were added to the ticket.
+            NSMutableArray *newWitnesses = [NSMutableArray arrayWithArray:[newTicket.helpedBy allObjects]];
+            [newWitnesses removeObjectsInArray:[_oldTicket.helpedBy allObjects]];
+            
+            // Removes all witnesses that no longer apply to the ticket.
+            for (Expert *expert in oldWitnesses) {
+                // Creates a new Witness object with this current expert and current ticket information. (Database Witnesses are
+                //  automatically updated with the new ticket number.)
+                Witness *oldWitness = [[Witness alloc] init];
+                [oldWitness setExpert_id:expert.expert_id];
+                [oldWitness setExpert:expert];
+                [oldWitness setTicket_no:newTicket.ticket_no];
+                [oldWitness setTicket:newTicket];
+                updated = [[DataController sharedDataController] removeWitness:oldWitness];
+                if (updated) {
+                    NSLog(@"Witness %@ %@ was removed.", expert.first_name, expert.last_name);
+                } else {
+                    NSLog(@"Witness %@ %@ was not removed.", expert.first_name, expert.last_name);
+                }
+            }
+            
+            for (Expert *expert in newWitnesses) {
+                // Creates a new Witness object with this current expert and ticket information. (Database Witnesses are
+                //  automatically updated with the new ticket number.)
+                Witness *newWitness = [[Witness alloc] init];
+                [newWitness setExpert_id:expert.expert_id];
+                [newWitness setExpert:expert];
+                [newWitness setTicket_no:newTicket.ticket_no];
+                [newWitness setTicket:newTicket];
+                updated = [[DataController sharedDataController] insertWitness:newWitness];
+                if (updated) {
+                    NSLog(@"Witness %@ %@ was added.", expert.first_name, expert.last_name);
+                } else {
+                    NSLog(@"Witness %@ %@ was not added.", expert.first_name, expert.last_name);
+                }
+            }
+            
             // Sets the old ticket to the updated ticket.
             _oldTicket = newTicket;
             // Updates all the fields to the new updated ticket.
@@ -452,9 +488,6 @@
     NSTextField *field = [notification object];
     NSString *identifer = [field identifier];
     NSString *text = [field stringValue];
-    
-    NSLog(@"ID: %@", identifer);
-    
     NSString *formatted;
     
     // Formats the string within the specified fields by adding a hyphen after so many characters. Will also
@@ -575,19 +608,6 @@
     }
 }
 
-//- (void)fillComboBox:(NSComboBox *)combo withItems:(NSArray *)items
-//{
-//    [combo removeAllItems];
-//    
-//    // Fills a combo box with all the items in the array.
-//    for (int x = 0; x < [items count]; x++) {
-//        NSString *first = [[items objectAtIndex:x] valueForKey:@"first_name"];
-//        NSString *last = [[items objectAtIndex:x] valueForKey:@"last_name"];
-//        NSString *name = [self formatFirstName:first lastName:last];
-//        [combo addItemWithObjectValue:name];
-//    }
-//}
-//
 - (void)fillExpertComboBoxes
 {
     // Fill in the "represenative" combo box.
@@ -613,145 +633,5 @@
     // Fill in the "other" combo box.
     [fieldFormatter fillComboBox:_otherCombo withItems:experts];
 }
-//
-//- (NSString *)formatFirstName:(NSString *)first lastName:(NSString *)last
-//{
-//    NSString *name;
-//    
-//    if (*_lastNameFirst == YES) {
-//        name = [NSString stringWithFormat:@"%@, %@", last, first];
-//    } else {
-//        name = [NSString stringWithFormat:@"%@ %@", first, last];
-//    }
-//    
-//    return [name capitalizedString];
-//}
-//
-//- (NSDictionary *)unformatName:(NSString *)name
-//{
-//    NSMutableArray *nameSplit;
-//    NSArray *keys;
-//    
-//    // Trims all spaces from beginning and end of name.
-//    name = [name stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-//    
-//    // Nothing was entered in, so return blanks for first and last name.
-//    if ([name isEqualToString:@""]) {
-//        return [NSDictionary dictionaryWithObjectsAndKeys:@"", @"first_name", @"", @"last_name", nil];
-//    }
-//    
-//    // Grabs the first and last name depending on the format.
-//    if (*_lastNameFirst == YES) {
-//        nameSplit = (NSMutableArray *)[name componentsSeparatedByString:@","];
-//        keys = [NSArray arrayWithObjects:@"last_name", @"first_name", nil];
-//    }
-//    else
-//    {
-//        nameSplit = (NSMutableArray *)[name componentsSeparatedByString:@" "];
-//        keys = [NSArray arrayWithObjects:@"first_name", @"last_name", nil];
-//    }
-//    
-//    // Removes all whitespace at the beginning and end of each name.
-//    for (int x = 0; x < nameSplit.count ; x++) {
-//        NSString *trimmedName = [nameSplit[x] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-//        [nameSplit replaceObjectAtIndex:x withObject:trimmedName];
-//    }
-//    
-//    // If there are more than 2 elements, that means the user typed in a middle name as well. We will combine the middle name with the first name.
-//    if (nameSplit.count > 2) {
-//        NSMutableString *combinedFirstName = [[NSMutableString alloc] init];
-//        NSString *lastName;
-//        
-//        if (*_lastNameFirst == YES) {
-//            // Combines all names except the first element. This will make a combined first name.
-//            for (int x = 1; x < nameSplit.count; x++) {
-//                [combinedFirstName appendString:nameSplit[x]];
-//                [combinedFirstName appendString:@" "];
-//            }
-//            lastName = nameSplit[0];
-//            
-//            // Removes spaces from the beginning and end of the combined first name.
-//            NSString *firstName = [combinedFirstName stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-//            // Adds the combined first name and last name into an array in the order that it will be added to the dictionary.
-//            [nameSplit removeAllObjects];
-//            [nameSplit addObject:lastName];
-//            [nameSplit addObject:firstName];
-//        }
-//        else {
-//            // Combined all names except the last element. This will make a combined first name.
-//            for (int x = 0; x < nameSplit.count-1; x++) {
-//                [combinedFirstName appendString:nameSplit[x]];
-//                [combinedFirstName appendString:@" "];
-//            }
-//            lastName = nameSplit[nameSplit.count-1];
-//            
-//            // Removes spaces from the beginning and end of the combined first name.
-//            NSString *firstName = [combinedFirstName stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-//            // Adds the combined first name and last name into an array in the order that it will be added to the dictionary.
-//            [nameSplit removeAllObjects];
-//            [nameSplit addObject:firstName];
-//            [nameSplit addObject:lastName];
-//        }
-//        
-//    }
-//    
-//    // Adds spaces to the array anywhere there is a missing component.
-//    if (nameSplit.count == 1) {
-//        [nameSplit addObject:@""];
-//    }
-//    
-//    // Creates a dictionary out of the names.
-//    NSDictionary *nameDict = [NSDictionary dictionaryWithObjects:nameSplit forKeys:keys];
-//    
-//    return nameDict;
-//}
-//
-//- (NSString *)callOrderBpaFormat:(NSString *)text
-//{
-//    text = [text stringByReplacingOccurrencesOfString:@"-" withString:@""];
-//    
-//    // Adds a '-' after every 4th and 7th character.
-//    NSMutableString *formatted = [[NSMutableString alloc] init];
-//    
-//    for (int x = 0; x < [text length]; x++) {
-//        if (x == 4 || x == 6) {
-//            [formatted appendString:@"-"];
-//        }
-//        NSString *nextChar = [NSString stringWithFormat:@"%c", [text characterAtIndex:x]];
-//        [formatted appendString:nextChar];
-//    }
-//    
-//    return formatted;
-//}
-//
-//- (NSString *)tinFormat:(NSString *)text
-//{
-//    text = [text stringByReplacingOccurrencesOfString:@"-" withString:@""];
-//    
-//    // Adds a '-' after the 4th character.
-//    NSMutableString *formatted = [[NSMutableString alloc] init];
-//    
-//    for (int x = 0; x < [text length]; x++) {
-//        if (x == 4) {
-//            [formatted appendString:@"-"];
-//        }
-//        NSString *nextChar = [NSString stringWithFormat:@"%c", [text characterAtIndex:x]];
-//        [formatted appendString:nextChar];
-//    }
-//    
-//    return formatted;
-//}
-//
-//- (void)setErrorBackground:(id)field
-//{
-//    // Sets the field's background to a slight red so that the user knows there is an error.
-//    [field setBackgroundColor:[NSColor colorWithRed:1 green:0 blue:0 alpha:0.20]];
-//}
-//
-//- (void)clearErrorBackground:(id)field
-//{
-//    // Reverts field to normal color.
-//    [field setBackgroundColor:[NSColor colorWithRed:0 green:0 blue:0 alpha:0]];
-//}
 
 @end
