21b767dfd1804b209769c3a08d9f1ab56731b6b8
Terrell707 <adr510909@gmail.com>
2015-05-06 13:07:33 -0700

    Table changes based on current selection in combobox.

--- SALT/BusinessViewController.h
+++ SALT/BusinessViewController.h
@@ -7,7 +7,24 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "DataController.h"
 
-@interface BusinessViewController : NSViewController
+@interface BusinessViewController : NSViewController <NSTableViewDelegate> {
+    IBOutlet NSArrayController *employeeController;
+    IBOutlet NSArrayController *controller;
+    IBOutlet NSTableView *businessTable;
+}
+
+@property NSMutableArray *employees;
+@property NSMutableArray *judges;
+@property NSMutableArray *sites;
+@property NSMutableArray *clerks;
+@property NSMutableArray *experts;
+
+@property (weak) IBOutlet NSComboBox *listOfTables;
+@property (weak) IBOutlet NSTextField *numRowsLabel;
+@property (weak) IBOutlet NSBox *infoBox;
+
+- (IBAction)businessCombo:(id)sender;
 
 @end
--- SALT/BusinessViewController.m
+++ SALT/BusinessViewController.m
@@ -16,7 +16,138 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do view setup here.
+    
+    _employees = [[NSMutableArray alloc] init];
+    _judges = [[NSMutableArray alloc] init];
+    _sites = [[NSMutableArray alloc] init];
+    _clerks = [[NSMutableArray alloc] init];
+    _experts = [[NSMutableArray alloc] init];
+    
+    controller = [[NSArrayController alloc] init];
+}
+
+- (void)viewDidAppear {
+    // Grabs the table that user chose to display last time this view was displayed.
+    NSInteger selectedIndex = [_listOfTables indexOfSelectedItem];
+    
+    NSLog(@"Selected Index: %ld", selectedIndex);
+    
+    // Checks to see if user selected something other than the initial table. If so, it reloads that table. Otherwise,
+    //  it loads the default table.
+    if (selectedIndex == -1) [self setEmployeeTable];
+    else [self selectTable];
+}
+
+- (IBAction)businessCombo:(id)sender {
+    [self selectTable];
+}
+
+- (void)selectTable
+{
+    NSInteger selectedIndex = [_listOfTables indexOfSelectedItem];
+    if (selectedIndex == 0) [self setEmployeeTable];
+    else if (selectedIndex == 1) [self setSiteTable];
+    else if (selectedIndex == 2) [self setJudgeTable];
+    else if (selectedIndex == 3) [self setSiteTable];
+    else if (selectedIndex == 4) [self setExpertTable];
+}
+
+- (void)setEmployeeTable
+{
+    _employees = [[DataController sharedDataController] employees];
+    
+    NSArray *columnIdentifers = [NSArray arrayWithObjects:@"emp_id", @"first_name", @"middle_init", @"last_name", @"phone_number", @"email", @"street", @"city", @"state", @"zip", @"pay", @"active", nil];
+    NSArray *columnNames = [NSArray arrayWithObjects:@"Emp ID", @"First Name", @"M.I.", @"Last Name", @"Phone Number",
+                            @"E-Mail", @"Street", @"City", @"State", @"Zip", @"Pay", @"Active", nil];
+    
+    [self changeTableWithColumnIdentifiers:columnIdentifers withNames:columnNames boundToData:_employees];
+    
+    [_infoBox setTitle:@"Employee Information"];
+}
+
+- (void)setSiteTable
+{
+    _sites = [[DataController sharedDataController] sites];
+    
+    NSArray *columnIdentifiers = [NSArray arrayWithObjects:@"office_code", @"name", @"address", @"email", @"can", @"pay", @"active", nil];
+    NSArray *columnNames = [NSArray arrayWithObjects:@"Office Code", @"Office Name", @"Address", @"E-Mail", @"CAN No.", @"Pay", @"Active", nil];
+    
+    [self changeTableWithColumnIdentifiers:columnIdentifiers withNames:columnNames boundToData:_sites];
+    
+    [_infoBox setTitle:@"Office Information"];
+}
+
+- (void)setJudgeTable
+{
+    _judges = [[DataController sharedDataController] judges];
+    
+    NSArray *columnIdentifiers = [NSArray arrayWithObjects:@"office", @"first_name", @"last_name", @"active", nil];
+    NSArray *columnNames  = [NSArray arrayWithObjects:@"Office", @"First Name", @"Last Name", @"Active", nil];
+    
+    [self changeTableWithColumnIdentifiers:columnIdentifiers withNames:columnNames boundToData:_judges];
+    
+    [_infoBox setTitle:@"Judge Information"];
+}
+
+- (void)setClerkTable
+{
+    // ADD CLERK ARRAY INFORMATION HERE!!!
+//    _clerks = [[DataController sharedDataController] clerks];
+    
+    NSArray *columnIdentifiers = [NSArray arrayWithObjects:@"first_name", @"last_name", @"email", nil];
+    NSArray *columnNames = [NSArray arrayWithObjects:@"First Name", @"Last Name", @"E-Mail", nil];
+    
+//    [self changeTableWithColumnIdentifiers:columnIdentifiers withNames:columnNames boundToData:_clerks];
+    
+    [_infoBox setTitle:@"Clerk Information"];
+}
+
+- (void)setExpertTable
+{
+    _experts = [[DataController sharedDataController] experts];
+    
+    NSArray *columnIdentifiers = [NSArray arrayWithObjects:@"first_name", @"last_name", @"role", @"active", nil];
+    NSArray *columnNames = [NSArray arrayWithObjects:@"First Name", @"Last Name", @"Role", @"Active", nil];
+    
+    [self changeTableWithColumnIdentifiers:columnIdentifiers withNames:columnNames boundToData:_experts];
+    
+    [_infoBox setTitle:@"Expert Information"];
+}
+
+- (void)changeTableWithColumnIdentifiers:(NSArray *)columnIdentifiers withNames:(NSArray *)columnNames boundToData:(NSArray *)data
+{
+    // Removes each column from the business table.
+    NSArray *columns = [businessTable tableColumns];
+    for (NSInteger x = columns.count - 1; x >= 0; x--) {
+        [businessTable removeTableColumn:columns[x]];
+    }
+    
+    // Binds the data to a controller so that the data will be displayed in the table.
+    [controller setContent:data];
+    
+    // Creates and adds columns to the table using the employee information.
+    for (NSInteger x = 0; x < columnIdentifiers.count; x++) {
+        NSTableColumn *column = [[NSTableColumn alloc] initWithIdentifier:columnIdentifiers[x]];
+        [column.headerCell setStringValue:columnNames[x]];
+        
+        NSString *keyPath = [NSString stringWithFormat:@"arrangedObjects.%@", columnIdentifiers[x]];
+        
+        [column bind:NSValueBinding toObject:controller withKeyPath:keyPath options:nil];
+        [column setHidden:NO];
+        [businessTable addTableColumn:column];
+    }
+    
+    // Binds the selected items in the table to the items they represent in the array controller.
+    [businessTable bind:@"selectionIndexes" toObject:controller withKeyPath:@"selectionIndexes" options:nil];
+    [businessTable setNeedsDisplay:YES];
+    [businessTable reloadData];
+    
+    // Binds the label to the number of items in the array controller.
+    NSString *label = @"%{value1}@ Rows";
+    [_numRowsLabel bind:@"displayPatternValue1"
+               toObject:controller
+            withKeyPath:@"arrangedObjects.@count"
+                options:@{NSDisplayPatternBindingOption : label}];
 }
 
 @end
