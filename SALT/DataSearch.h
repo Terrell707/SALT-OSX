e35acbe2f1f481abaeab67bd8421efd2b27990ff
Terrell707 <adr510909@gmail.com>
2015-05-17 19:49:18 -0700

    Pulled search functionality to its own file.

---
+++ SALT/DataSearch.h
@@ -0,0 +1,13 @@
+//
+//  DataSearch.h
+//  SALT
+//
+//  Created by Adrian T. Chambers on 5/17/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataSearch : NSObject
+
+@end
---
+++ SALT/DataSearch.m
@@ -0,0 +1,13 @@
+//
+//  DataSearch.m
+//  SALT
+//
+//  Created by Adrian T. Chambers on 5/17/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import "DataSearch.h"
+
+@implementation DataSearch
+
+@end
