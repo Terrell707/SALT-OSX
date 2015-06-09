fb09a3e054e6fc7e70eae642ef9ecd83130da20c
Terrell707 <adr510909@gmail.com>
2015-06-08 19:32:39 -0700

    New tests.

---
+++ SALTTests/DataSearchTests.m
@@ -0,0 +1,40 @@
+//
+//  DataSearchTests.m
+//  SALT
+//
+//  Created by Adrian T. Chambers on 5/28/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface DataSearchTests : XCTestCase
+
+@end
+
+@implementation DataSearchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
