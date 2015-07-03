00ff114b34cc5feda434a218b270c3243095cac1
Terrell707 <adr510909@gmail.com>
2015-07-02 20:17:47 -0700

    Initial View for updating business info

---
+++ SALT/BusinessUpdateViewController.h
@@ -0,0 +1,13 @@
+//
+//  BusinessUpdateViewController.h
+//  SALT
+//
+//  Created by Adrian T. Chambers on 7/2/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BusinessUpdateViewController : NSViewController
+
+@end
---
+++ SALT/BusinessUpdateViewController.m
@@ -0,0 +1,22 @@
+//
+//  BusinessUpdateViewController.m
+//  SALT
+//
+//  Created by Adrian T. Chambers on 7/2/15.
+//  Copyright (c) 2015 Adrian T. Chambers. All rights reserved.
+//
+
+#import "BusinessUpdateViewController.h"
+
+@interface BusinessUpdateViewController ()
+
+@end
+
+@implementation BusinessUpdateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
